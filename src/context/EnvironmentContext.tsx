201fdadeaa24da4f498682b8503cfe0160eb50d3
Jonas Lindenskov Nielsen <jonas.lindenskov@criipto.com>
2024-06-28 13:30:39 +0200

    Fix linting

--- src/context/EnvironmentContext.tsx
+++ src/context/EnvironmentContext.tsx
@@ -1,10 +1,4 @@
-import {
-  ReactNode,
-  createContext,
-  useContext,
-  useEffect,
-  useState,
-} from 'react';
+import { ReactNode, createContext, useContext, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 export type Environment = 'test' | 'production';
